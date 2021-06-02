bb157170b9fee1845efd5ccbb000ecb2fb3f7a71
useregistrado <oscar.vargas07@uptc.edu.co>
2021-06-02 17:00:49 -0500

    version 0.9 con validacion por alerts con js del formulario, integracion del testimonio e integracion de frase superior del nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,23 @@
-//alert("La resolución de tu pantalla es: " + screen.width + " x " + screen.height)
- // javascript:location.reload(true)
+// alert("La resolución de tu pantalla es: " + screen.width + " x " + screen.height)
+//javascript:location.reload(true)
+
+function validarForm() {
+  var nombre = document.getElementById('nombre_id').value
+  var ciudad = document.getElementById('ciudad_id').value
+  var telefono = document.getElementById('telefono_id').value
+  var email = document.getElementById('email_id').value
+  var check = document.getElementById('terminos_id')
+  if(nombre === ''){
+    alert("Por favor rellene el campo nombre")
+  }else if (ciudad === '') {
+    alert("Por favor rellene el campo ciudad")
+  }else if (telefono === '') {
+    alert("Por favor rellene el campo teléfono")
+  }else if (email === '') {
+    alert("Por favor rellene el campo emil")
+  }else if (!check.checked==1) {
+    alert("Por favor acepte los términos")
+  }else {
+    document.getElementById('form_id').submit()
+  }
+}
